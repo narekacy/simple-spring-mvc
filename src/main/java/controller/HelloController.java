829afdfc7119533d10dc892455d41b55a2c3975b
Narek <gurjiev42@gmail.com>
2020-02-06 13:11:36 +0200

    commit message

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+}
